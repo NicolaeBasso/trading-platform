efcd5cb5a96e7203704ef4a7926727700e16c1d7
Nicolae Basso <nickbassotrue@gmail.com>
2022-12-14 00:45:46 +0200

    init monorepo

---
+++ trading-platform-auth/src/health/health.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
+import { HealthController } from './health.controller';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [TerminusModule, HttpModule, ConfigModule],
+  controllers: [HealthController],
+  providers: [],
+})
+export class HealthModule {}
