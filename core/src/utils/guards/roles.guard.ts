54fd5c4ebfce33b168e25a6f9aae2ff472e7c1c9
Nicoale Basso <nickbassotrue@gmail.com>
2023-02-04 01:14:54 +0200

    mongodb clustering setup, 1 replica per 1 service usage; postman collection added to misc

--- core/src/utils/guards/roles.guard.ts
+++ core/src/utils/guards/roles.guard.ts
@@ -9,11 +9,16 @@ import {
 import { Observable } from 'rxjs';
 import { JwtService } from '@nestjs/jwt';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from '../../utils/constants';
+import { ConfigService } from '@nestjs/config';
+import { ROLES_KEY } from 'src/utils/constants';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+    private configService: ConfigService,
+  ) {}
 
   canActivate(
     context: ExecutionContext,
@@ -30,19 +35,26 @@ export class RolesGuard implements CanActivate {
       const authHeader = req.headers.authorization;
 
       const bearer = authHeader?.split(' ')[0];
-      const token = authHeader?.split(' ')[1];
+      // const token = authHeader?.split(' ')[1];
+      const token = req.cookies?.token;
 
-      if (bearer !== 'Bearer' || !token) {
+
+      if (!token) {
         throw new UnauthorizedException({ message: 'Unauthorized' });
       }
 
-      const user = this.jwtService.verify(token);
+      let user = null;
+      try {
+        user = this.jwtService.verify(token, {
+          secret: this.configService.get<string>('JWT_SECRET'),
+        });
+      } catch (error) {
+        console.error(error);
+      }
       req.user = user;
 
-      console.log({ roles: user.roles });
-      console.log({ requiredRoles });
-
-      return user.roles.some((role) => requiredRoles.includes(role.value));
+      // return user.role.some((role) => requiredRoles.includes(role.value));
+      return user.role === requiredRoles[0];
     } catch (e) {
       throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
     }
--- startdb.sh
+++ startdb.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
 # run this before anything else
-# chmod +x startdb.sh
+chmod +x startdb.sh
 
 # to start all apps
 # ./startdb.sh
 
-docker-compose up -d
-
-sleep 10
-
+docker-compose up -d --build --force-recreate
+sleep 30
 docker exec mongo1 /scripts/rs-init.sh
+
